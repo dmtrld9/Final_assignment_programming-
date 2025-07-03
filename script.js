a601856f79e77699e2506af1f7f0e0e4e5581531
dmtrld9 <dmitrik3007@gmail.com>
2025-07-04 00:19:51 +0200

    Add files via upload
    Performed a JavaScript Validation + Logic:

---
+++ script.js
@@ -0,0 +1,39 @@
+document.getElementById('myForm').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const name = document.getElementById('name').value.trim();
+  const age = document.getElementById('age').value.trim();
+  const email = document.getElementById('email').value.trim();   
+  const messageDiv = document.getElementById('message');
+  const list = document.getElementById('myList');
+  const listItem = document.createElement('li');
+
+  if (validateInput(name, age, email)) {
+    messageDiv.textContent = "You registered successfully!";
+    messageDiv.style.color = "green";
+
+    
+    listItem.textContent = `Name:${name}, Age: ${age}, Email: ${email})`;
+    list.appendChild(listItem);
+
+    document.getElementById('myForm').reset();
+  } else if (Number(age) < 20) {
+    messageDiv.textContent = "Your age must be more than 18";
+    messageDiv.style.color = "red";
+  } else {
+    messageDiv.textContent = "Please fill all fields correctly";
+    messageDiv.style.color = "red";
+  }
+});
+
+function validateInput(name, age, email) {
+  if (!name || !age || !email) return false;
+
+  const ageNum = Number(age);
+  const validAge = ageNum > 18;
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+  const validEmail = emailRegex.test(email);
+
+  return validAge && validEmail;
+}
